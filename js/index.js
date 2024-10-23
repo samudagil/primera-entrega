ea34f044bccd734814a0b504aaddd5bdc11acbc6
samuel gil <samuelgil101215@gmail.com>
2024-10-23 11:16:12 +0200

    reentrega

--- js/index.js
+++ js/index.js
@@ -9,26 +9,55 @@ let tvTesla = 299;
 function producto (tv){
     if(tv.toLocaleLowerCase() === "lg"){
         console.log("el monto de su producto es " + tvLg + "$");
+        function montoProducto(cuotas){
+            let i = 1;
+            while(i <= cuotas){
+                let divisionMontoFinal = tvLg / cuotas;
+                console.log("el monto a pagar es de " + divisionMontoFinal.toFixed(2) + "$ en " + cuotas + " meses")
+                break;
+        }
+        }
+        
+    montoProducto(parseInt(prompt("ingrese en cuantas cuotas desea pagar")))
     }else if(tv.toLocaleLowerCase() === "samsung"){
         console.log("el monto de su producto es " + tvSamsung + "$");
+        function montoProducto(cuotas){
+            let i = 1;
+            while(i <= cuotas){
+                let divisionMontoFinal = tvSamsung / cuotas;
+                console.log("el monto a pagar es de " + divisionMontoFinal.toFixed(2) + "$ en " + cuotas + " meses")
+                break;
+        }
+        }
+        
+    montoProducto(parseInt(prompt("ingrese en cuantas cuotas desea pagar")))
     }else if(tv.toLocaleLowerCase() === "xiaomi"){
         console.log("el monto de su producto es " + tvXiaomi + "$");
+        function montoProducto(cuotas){
+            let i = 1;
+            while(i <= cuotas){
+                let divisionMontoFinal = tvXiaomi / cuotas;
+                console.log("el monto a pagar es de " + divisionMontoFinal.toFixed(2) + "$ en " + cuotas + " meses")
+                break;
+        }
+        }
+        
+    montoProducto(parseInt(prompt("ingrese en cuantas cuotas desea pagar")))
     }else if(tv.toLocaleLowerCase() === "tesla"){
         console.log("el monto de su producto es " + tvTesla + "$");
+        function montoProducto(cuotas){
+            let i = 1;
+            while(i <= cuotas){
+                let divisionMontoFinal = tvTesla / cuotas;
+                console.log("el monto a pagar es de " + divisionMontoFinal.toFixed(2) + "$ en " + cuotas + " meses")
+                break;
+        }
+        }
+        
+    montoProducto(parseInt(prompt("ingrese en cuantas cuotas desea pagar")))
     }else{
         console.log("el producto que ingreso no es correcto");
     }
 }
 
 producto(prompt("ingrese marca de tlevision(LG, SAMSUNG, XIAOMI, TESLA)"));
-
-function pagoCuotas(montoProducto, cuotas){
-    let i = 1;
-    while(i <= cuotas){
-        let divisionMontoFinal = montoProducto / cuotas;
-        console.log("el monto a pagar es de " + divisionMontoFinal.toFixed(2) + "$ en " + cuotas + " meses")
-        break;
-    }
-}
-
-pagoCuotas(parseInt(prompt("ingrese monto del producto")),parseInt(prompt("ingrese en cuantas cuotas desea pagar")));
